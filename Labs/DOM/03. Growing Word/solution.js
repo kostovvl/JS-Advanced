bfce1dd1aefbcd41b8fb4b54d5e75e303a0e103a
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-20 12:04:27 +0300

    DOM Lab Ex.3

--- Labs/DOM/03. Growing Word/solution.js
+++ Labs/DOM/03. Growing Word/solution.js
@@ -1,4 +1,30 @@
 function growingWord() {
 
-  //TODO...
+  let growingWord =
+   document.querySelector('#exercise > p');
+   let px = 8;
+   let colorChanges = {
+     'blue' : 'green',
+     'green' : 'red',
+     'red' : 'blue'
+   }
+
+   if (!growingWord.hasAttribute("style")) {
+     growingWord.setAttribute("style", `color: blue; font-size: ${px}px`) 
+   } else {
+     let currentPx = growingWord.style["font-size"];
+      px = currentPx.substring(0, currentPx.length - 2) * 2;
+      if (px >= 64) {
+        px = 8;
+      }
+     let currentColor = growingWord.style["color"];
+
+     
+ 
+
+     growingWord.setAttribute("style", 
+     `color : ${colorChanges[currentColor]}; font-size : ${px}px`)
+   }
+
+  
 }
