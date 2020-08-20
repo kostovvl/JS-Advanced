fd2aba786220865b8560876e180b3382fafd17da
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-20 12:55:45 +0300

    DOM Lab Ex.4

--- Labs/DOM/04. Visited Websites/solution.js
+++ Labs/DOM/04. Visited Websites/solution.js
@@ -1,4 +1,24 @@
 function solve() {
+const anchors = Array.from(document.querySelectorAll('a'));
+const counters = anchors.map(a => +a.nextElementSibling.
+  innerText.split(' ')[1]);
+const container = document.
+querySelector('#page1 > .middled')
 
-  //TODO...
+container.addEventListener('click', function(e){
+  const target = e.target;
+  const parent = e.target.parentElement;
+  const notTargetAnchor = target.localName !== 'a';
+  const notTagretParentAnchor = (parent && parent.localName !== 'a');
+
+  if (notTargetAnchor && notTagretParentAnchor) {return};
+  const targetAnchor = notTargetAnchor ? parent : target;
+  const counterIndex = anchors.indexOf(targetAnchor);
+
+  if (counterIndex === -1) {return};
+  counters[counterIndex]++;
+  const paregraph = targetAnchor.nextElementSibling;
+  paregraph.innerText = `visited ${counters[counterIndex]} times`
+})
+
 }
