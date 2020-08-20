ef24945994e7009688807db4325847f4ed91289d
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-20 11:03:53 +0300

    DOM Ex.1

--- Labs/DOM/01. Articles List/solution.js
+++ Labs/DOM/01. Articles List/solution.js
@@ -1,4 +1,32 @@
 function createArticle() {
+
+
+	let createTitle = 
+	document.getElementById('createTitle')
+	let createContent = 
+	document.getElementById('createContent');
+	let articleSection =
+    document.getElementById('articles')
+
+	let titleText = createTitle.value;
+	let contentText = createContent.value;
+
+	if (!titleText || !contentText) {
+		return
+	}
+
+	let newArticle = document.createElement('article');
+	let newTitle = document.createElement('h3')
+	let newContent = document.createElement('p');
+
+	newArticle.appendChild(newTitle);
+	newArticle.appendChild(newContent);
+
+	newContent.innerText = contentText;
+	newTitle.innerText = titleText;
 	
-	//TODO...
+	articleSection.appendChild(newArticle);
+
+	createTitle.value = '';
+	createContent.value = '';
 }
