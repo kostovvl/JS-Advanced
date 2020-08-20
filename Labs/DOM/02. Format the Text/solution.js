51b49b08ce65175af5aa7164db16e7c089c0adb9
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-20 11:31:17 +0300

    DOM Lab Ex.2

--- Labs/DOM/02. Format the Text/solution.js
+++ Labs/DOM/02. Format the Text/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO...
+  const input =
+   document.querySelector('#input');
+  const outpurString =
+   document.querySelector('#output');
+
+   const inputStringArr = 
+   input.innerText.split('. ');
+
+   let inputParagraph = document.createElement('p');
+   let counter = 0;
+   for(let i = 0; i < inputStringArr.length; i++) {
+     counter++;
+     inputParagraph.innerText += inputStringArr[i];
+     if (counter % 3 === 0) {
+       outpurString.appendChild(inputParagraph);
+       inputParagraph = document.createElement('p');
+       counter = 0;
+     }
+   }
+
+   if (counter > 0) {
+    outpurString.appendChild(inputParagraph);
+   }
 }
