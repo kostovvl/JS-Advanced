264b25395fcd56499aa4876067c3b5727ae0baf3
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-22 20:46:05 +0300

    DOM Manipulation Lab Ex.5

--- Labs/DOM Manipulations/05. Mouse-Gradient/app.js
+++ Labs/DOM Manipulations/05. Mouse-Gradient/app.js
@@ -1,3 +1,22 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+   let gradient = document.querySelector('#gradient');
+    
+
+   gradient.addEventListener('mousemove', onMove);
+    gradient.addEventListener('mouseout', onOut);
+
+    let result = document.querySelector('#result');
+
+    function onMove(e) {
+        let coordinate = Math.round(e.clientX / 3);
+        let resultStr = `${coordinate}%`;
+
+        result.innerHTML = resultStr;
+
+    }
+
+    function onOut(e) {
+        result.innerHTML = '';
+    }
+    
 }
