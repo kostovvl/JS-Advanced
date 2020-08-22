1b8a000a8d43adc3dd679312645df1b591c9e76b
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-22 19:02:15 +0300

    DOM Monipulaiton Lab Ex. 3

--- Labs/DOM Manipulations/03. Delete-From-Table/app.js
+++ Labs/DOM Manipulations/03. Delete-From-Table/app.js
@@ -1,3 +1,32 @@
+
+
 function deleteByEmail() {
-    console.log('TODO:...');
+    let customers = 
+Array.from(document.querySelector('#customers')
+.querySelector('tbody')
+.querySelectorAll('tr'));
+    let eMailInput = document.querySelector('input[name="email"]');
+    let eMail = eMailInput.value;
+
+    let exists = false;
+
+    for (let i = 0; i < customers.length; i++) {
+        let customer = customers[i];
+        let emailNode = 
+        customer.childNodes[3];
+
+        if (emailNode.innerText === eMail) {
+          document.querySelector('#customers')
+            .querySelector('tbody').deleteRow(i)
+            exists = true;
+        }
+    }
+
+    if (!exists) {
+        alert('User with such email does not exist!');
+        eMailInput.value = '';
+        return;
+    }
+
+    eMailInput.value = '';
 }
