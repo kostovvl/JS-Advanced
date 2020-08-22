9126192489a82c8e0ae3b6e32fb99c65d8a03b48
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-22 18:18:26 +0300

    DOM Manipualtion Lab Ex.2

--- Labs/DOM Manipulations/02. Add-Delete/app.js
+++ Labs/DOM Manipulations/02. Add-Delete/app.js
@@ -1,3 +1,28 @@
+
+
 function addItem() {
-    //TODO...
+    let items = 
+document.querySelector('#items');
+    const newItem = document.querySelector('#newText')
+    const newItemText = newItem.value;
+    let newListElement = document.createElement('li');
+    newListElement.innerText = newItemText;
+    let deleteOption = document.createElement('a');
+    deleteOption.innerText = ('[Delete]');
+    deleteOption.href = '#'
+    items.addEventListener('click', onDelete);
+    newListElement.appendChild(deleteOption);
+    items.appendChild(newListElement);
+
+    newItem.value = '';
+
+    function onDelete(e) {
+        e.preventDefault;
+       const target = e.target;
+       const parent = target.parentElement;
+       parent.remove();
+    }
 }
+
+
+
