14e3fb1979d20e367b313761105e61b60dcb5b1d
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-22 19:26:47 +0300

    DOM manipulation Lab Ex.3

--- Labs/DOM Manipulations/04. Stopwatch/stopwatch.js
+++ Labs/DOM Manipulations/04. Stopwatch/stopwatch.js
@@ -1,3 +1,38 @@
 function stopwatch() {
-    console.log('TODO:...');
+let startButton = document.querySelector('#startBtn')
+let stopButton = document.querySelector('#stopBtn');
+
+startButton.addEventListener('click', onStart);
+stopButton.addEventListener('click', onStop);
+
+let time = document.querySelector('#time');
+let id;
+
+    function onStart(e) {
+        let seconds = 0;
+        let minutes = 0;
+        startButton.disabled = true;
+        stopButton.disabled = false;
+        id = setInterval(function () {
+            seconds++;
+            if (seconds === 60) {
+                seconds = 0;
+                minutes++;
+            }
+            if (minutes === 60) {
+                minutes = 0;
+            }
+            time.innerText = `${minutes} : ${seconds}`;
+        }, 1000); 
+    }
+
+
+
+    function onStop(e) {
+        startButton.disabled = false;
+        stopButton.disabled = true;
+        clearTimeout(id);
+    }
+
+
 }
