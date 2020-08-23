a9faf8c5b28d606e0130e175fb037f040b615047
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-23 16:50:19 +0300

    DOM Manipulations Lab Ex7

--- Labs/DOM Manipulations/06. Highlight-Active/app.js
+++ Labs/DOM Manipulations/06. Highlight-Active/app.js
@@ -1,3 +1,24 @@
 function focus() {
-    console.log('TODO:...');
+    let inputSections =
+    document.querySelector('#input-sections');
+    
+    inputSections.addEventListener('mouseover', onMouseOver);
+    inputSections.addEventListener('mouseout', onMouseOut);
+    
+     
+     function onMouseOver(e) {
+         let currentSelector = e.target;
+         
+         if (currentSelector.nodeName === 'INPUT') {
+             currentSelector.parentNode.style.background = "grey"
+         }
+     }
+
+     function onMouseOut(e) {
+        let currentSelector = e.target;
+        if (currentSelector.nodeName === 'INPUT') {
+            currentSelector.parentNode.style.background = "white"
+        }
+     }
+    
 }
--- Labs/DOM Manipulations/07. Dynamic-Validation/app.js
+++ Labs/DOM Manipulations/07. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+    let inputField = 
+    document.querySelector('#email');
+    inputField.addEventListener('change', onChange);
+    inputField.addEventListener('click', onClick);
+
+    function onChange(e) {
+        let email = inputField.value;
+        if (!email.match('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}')) {
+            inputField.classList.add('error');
+        }
+    }
+
+    function onClick(e) {
+        inputField.classList.remove('error');
+        inputField.value = ''; 
+    }
 }
