f5d0d822ddb9d02ebce874f2452351c919bc99a4
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-23 20:24:09 +0300

    DOM Manipulations Exercise Ex4

--- Exercises/DOM Manipulation/04. Locked-Profile/app.js
+++ Exercises/DOM Manipulation/04. Locked-Profile/app.js
@@ -1,3 +1,21 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let main = 
+    document.querySelector('main');
+
+    main.addEventListener('click', onClick);
+
+    function onClick(e) {
+        let hiddenfield = 
+        e.target.parentNode.children[9];
+
+        let property = hiddenfield.style.display;
+
+        if (property === '') {
+            hiddenfield.style.display = 'block';
+            e.target.textContent = 'Show Less'
+        } else {
+            hiddenfield.style.display = 'none';
+            e.target.textContent = 'Show More'
+        }
+    }
 }
