75454db16c8b715522e63978d1fca9ca33c04a3b
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-23 23:09:45 +0300

    DOM Manopulations Exercise Ex6

--- Exercises/DOM Manipulation/06. Table-Search-Engine/solution.js
+++ Exercises/DOM Manipulation/06. Table-Search-Engine/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-   //TODO...
+   document.querySelector('#searchBtn')
+   .addEventListener('click', onClick);
+   let tableRoles = Array.from(document.querySelector('tbody').children);
+
+   function onClick() {
+      let searchBar = document.querySelector('#searchField')
+      let search = searchBar.value; 
+      tableRoles.forEach(tableRow => {
+        let children = Array.from(tableRow.children);
+        children.forEach(child => {
+            if (child.textContent.includes(search)) {
+              tableRow.classList.add('select')
+            }
+         })
+      });
+      searchBar.value = '';
+     }
+   document.querySelector('#searchField')
+     .addEventListener('click', onClick2);
+
+     function onClick2() {
+      tableRoles.forEach(tableRow => {
+               tableRow.classList.remove('select')
+       });
+     }
 }
