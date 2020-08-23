1983ede4338b6e258afb428e5a09cef310191b3a
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-08-23 20:39:33 +0300

    DOM Manipulations Exercise Ex5

--- Exercises/DOM Manipulation/05. Encode-and-Decode-Messages/app.js
+++ Exercises/DOM Manipulation/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,41 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    document.querySelector('#encode').addEventListener('click', onEncode);
+    document.querySelector('#decode').addEventListener('click', onDecode);
+
+
+    let inputTextArea = document.querySelector('#text-encode');
+    let outputTextArea = document.querySelector('#text-decode');
+    
+    function onEncode(e) {
+        let input = inputTextArea.value;
+
+        outputTextArea.value = encode(input);
+    }
+
+    function onDecode(e) {
+        let encodedText = outputTextArea.value;
+        outputTextArea.value = decode(encodedText);
+    }
+
+    function encode(input) {
+        let result = [];
+
+        for(let i = 0; i < input.length; i++) {
+            let currentchar = input.charCodeAt(i) + 1;
+            result.push(String.fromCharCode(currentchar))
+        }
+
+        return result.join('');
+    }
+
+    function decode(input) {
+        let result = [];
+
+        for(let i = 0; i < input.length; i++) {
+            let currentchar = input.charCodeAt(i) - 1;
+            result.push(String.fromCharCode(currentchar))
+        }
+
+        return result.join('');
+    }
 }
